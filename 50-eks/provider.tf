bea72a57096b054059e69414c2a205601f74ea37
prameela-namala <prameelanamala@gmail.com>
2025-03-28 00:28:52 +0530

    infra

--- 50-eks/provider.tf
+++ 50-eks/provider.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.75.0"
+      version = "5.66.0"
     }
   }
 
   backend "s3" {
     bucket = "expense-infra-dev2"
-    key    = "expense-eks"
+    key    = "expense-eks-dev"
     region = "us-east-1"
     dynamodb_table = "infra-lock-2"
   }
